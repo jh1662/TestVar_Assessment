3c9b71e0eea8eefc2fb09830f753ba6468019a56
jh1662 <JH1662@canterbury.ac.uk>
2024-10-20 13:46:40 +0100

    finally able to host server on http://localhost:3000

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,7 @@
+
+
 //: set up and instantiate the REST API (express.js)
-const express = require('express')();
+const express = require('express');
 const app = express();
 app.use(express.json());
 //^ instantiating this way imports the ability to parse JSONs
@@ -10,3 +12,11 @@ app.listen(
     //^ lambda function that notify when the server is up and the URL to access it
 
 //: Corrosponds websites to URLs
+
+/* DEBUG LOG:
+> Fixed error: "TypeError: Cannot read properties of undefined (reading 'headersSent')"
+- Used guide: "https://stackoverflow.com/questions/51125444/nodejs-typeerror-cannot-read-property-headerssent-of-undefined"
+- Removed "()" from "const express = require('express')();"
+
+>
+*/

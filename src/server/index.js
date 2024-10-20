2fa759774f41cd905615c7149cbaebbd022c6908
jh1662 <JH1662@canterbury.ac.uk>
2024-10-20 14:33:08 +0100

    Abe to repond to requests with html files using the "path" module.
    Intalled "nodemon" ,for hot-refreshing when developing, and "jest" for a testing framework for white-box testing. Both are installed with the "--save-dev" command to spaerate the modules from those that are used exclusively to run the code.

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,3 @@
-
-
 //: set up and instantiate the REST API (express.js)
 const express = require('express');
 const app = express();
@@ -11,12 +9,24 @@ app.listen(
     () => console.log(`Server is up on URL: http://localhost:${port}`))
     //^ lambda function that notify when the server is up and the URL to access it
 
+//: the rest of the setup
+const path = require('path');
+//^ allows fetching of other files
+
 //: Corrosponds websites to URLs
+app.get('/', function(req, res) {
+    //* home page
+    res.sendFile(path.join(__dirname, '../client/home.html'));
+});
 
 /* DEBUG LOG:
 > Fixed error: "TypeError: Cannot read properties of undefined (reading 'headersSent')"
-- Used guide: "https://stackoverflow.com/questions/51125444/nodejs-typeerror-cannot-read-property-headerssent-of-undefined"
+- Used guide: https://stackoverflow.com/questions/51125444/nodejs-typeerror-cannot-read-property-headerssent-of-undefined
 - Removed "()" from "const express = require('express')();"
 
 >
+*/
+
+/* Guides used:
+> Beginner guide of Express.js: https://www.youtube.com/watch?v=-MTSQjw5DrM
 */
